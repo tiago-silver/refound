2891ff3b2abe43fa2b8af03b5395111a982319b5
Tiago Silva <tiagopereirasilva5425@gamil.com>
2025-05-07 18:20:24 -0300

    Criada a validação do refunds

--- src/controllers/refunds-controller.ts
+++ src/controllers/refunds-controller.ts
@@ -1,8 +1,19 @@
 import { Request, Response } from "express";
+import { z } from "zod";
+
+const categoryEnum = z.enum(["food", "transport", "services", "accommodation", "others"])
 
  class RefundsController {
     async create(request: Request, response: Response) {
 
+        const bodySchema = z.object({
+            name: z.string().trim().min(3, {message: "Informe o nome da solicitação!"}),
+            amount: z.number().positive({message: "O valor deve ser positivo!"}),
+            category: categoryEnum,
+            filename: z.string().min(20),
+        })
+
+        const { name, amount, category, filename } = bodySchema.parse(request.body)
         return response.json({message: "ok"})
     }
 }

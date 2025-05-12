dadca6b763f611b5ce1a596cba6ede77cebfe0b6
Tiago Silva <tiagopereirasilva5425@gamil.com>
2025-05-12 19:22:50 -0300

    Terminada as configurações de uploads

--- src/app.ts
+++ src/app.ts
@@ -3,11 +3,16 @@ import express from 'express';
 import cors from 'cors';
 import { errorHandling } from './middlewares/error-handling';
 import { routes } from './routes';
+import uploadConfig from './config/upload';
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
+
+// Rotas para visualizar os arquivos carregados
+app.use("/uploads", express.static(uploadConfig.UPLOADS_FOLDER));
+
 app.use(routes);
 
 app.use(errorHandling)
